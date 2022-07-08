05c3ae660539d7064b4527bf49f5fb2228aa3b95
Liza Volyanskaya <you@example.com>
2022-07-08 15:52:33 +0300

    commit3

---
+++ HW_02/js/my.js
@@ -0,0 +1,7 @@
+var firstName = prompt ("Enter Your  first name");
+console.log (firstName);
+var lastName = prompt ("Enter Your  last name")
+console.log (lastName);
+var yearOfBirth = prompt ("Enter Your year of birth  ");
+var age = 2022 - yearOfBirth;
+console.log (age);
