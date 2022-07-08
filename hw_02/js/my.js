861525002127553a0f78d89f176626525436f5b0
Liza Volyanskaya <you@example.com>
2022-07-08 14:26:13 +0300

    Init Message

--- hw_02/js/my.js
+++
@@ -1,9 +0,0 @@
-
-var firstName = prompt ("Enter Your  first name");
-console.log (firstName);
-var lastName = prompt ("Enter Your  last name")
-console.log (lastName);
-var yearOfBirth = prompt ("Enter Your year of birth  ");
-var age = 2022 - yearOfBirth;
-console.log (age);
-
